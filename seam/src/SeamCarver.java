20ae0d29e1a4ad65789d545487f1cf6ed13d0f5e
wangj <wangjingwj@gmail.com>
2021-12-26 02:45:12 +0000

    energy ok

--- seam/src/SeamCarver.java
+++ seam/src/SeamCarver.java
@@ -1,10 +1,23 @@
 import edu.princeton.cs.algs4.Picture;
+import java.awt.Color;
+import java.net.NetPermission;
 
 public class SeamCarver {
 
     private final Picture picture;
 
-
+    private void validateColumnIndex(int i) {
+        if (i < 0 || i >= picture.width()) throw new IllegalArgumentException("Column index");
+    }
+    private void validateRowIndex(int i) {
+        if (i < 0 || i >= picture.height()) throw new IllegalArgumentException("Row index");
+    }
+    private long energy(Color a, Color b) {
+        long sum = (a.getRed()-b.getRed())*(a.getRed()-b.getRed());
+        sum += (a.getGreen()-b.getGreen())*(a.getGreen()-b.getGreen());
+        sum += (a.getBlue()-b.getBlue())*(a.getBlue()-b.getBlue());
+        return sum;
+    }
     // create a seam carver object base one the given picture
     public SeamCarver(Picture picture) {
         this.picture = picture;
@@ -27,7 +40,13 @@ public class SeamCarver {
 
     // energy of pixel at column x and row y
     public double energy(int x, int y){
-        return 0.0;
+        validateColumnIndex(x);
+        validateRowIndex(y);
+        if (x == 0 || y == 0) return 1000;
+        if (x == picture.width()-1 || y == picture.height()-1) return 1000;
+        long columnEnergy = energy(picture.get(x, y-1), picture.get(x, y+1));
+        long rowEnergy = energy(picture.get(x-1, y), picture.get(x+1, y));
+        return Math.sqrt(columnEnergy + rowEnergy);
     }
 
     // sequence of indices for horizontal seam
--- seam/test/EnergyUnitTest.java
+++ seam/test/EnergyUnitTest.java
@@ -1,3 +1,6 @@
+import java.awt.Color;
+import java.math.BigDecimal;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -8,7 +11,7 @@ public class EnergyUnitTest {
 
     @Test
     public void testLoadPicture() {
-        Picture p = new Picture("data/3-4.png");
+        Picture p = new Picture("data/3x4.png");
         Assertions.assertEquals(3, p.width());
         Assertions.assertEquals(4, p.height());
         Assertions.assertEquals(-13159, p.getRGB(1, 2));
@@ -16,7 +19,47 @@ public class EnergyUnitTest {
 
     @Test
     public void testLoadRGB() {
-        Picture p = new Picture("data/3-4.png");
+        Picture p = new Picture("data/3x4.png");
+        Color c = p.get(1, 2);
+        Assertions.assertEquals(255, c.getRed());
+        Assertions.assertEquals(204, c.getGreen());
+        Assertions.assertEquals(153, c.getBlue());
+        c = p.get(2, 3);
+        Assertions.assertEquals(255, c.getRed());
+        Assertions.assertEquals(255, c.getGreen());
+        Assertions.assertEquals(255, c.getBlue());
+    }
+
+    @Test 
+    public void testEdgeEnergy() {
+        Picture p = new Picture("data/3x4.png");
+        SeamCarver sc = new SeamCarver(p);
+        Assertions.assertEquals(1000, sc.energy(0, 0));
+        Assertions.assertEquals(1000, sc.energy(1, 3));
+        Assertions.assertEquals(1000, sc.energy(0, 3));
+        
+        p = new Picture("data/6x5.png");
+        sc = new SeamCarver(p);
+        Assertions.assertEquals(1000, sc.energy(0, 0));
+        Assertions.assertEquals(1000, sc.energy(0, 3));
+        Assertions.assertEquals(1000, sc.energy(5, 4));
+        Assertions.assertEquals(1000, sc.energy(4, 4));
+
+
+    }
+
+    @Test
+    public void testCenterEnergy() {
+        Picture p = new Picture("data/3x4.png");
+        SeamCarver sc = new SeamCarver(p);
+        Assertions.assertEquals(228.08770243044668, sc.energy(1, 2));
+        Assertions.assertEquals(228.52789764052878, sc.energy(1, 1));
+
+        p = new Picture("data/6x5.png");
+        sc = new SeamCarver(p);
+        Assertions.assertEquals(151.02, Math.round(sc.energy(2, 1)*100.0)/100.0);
+        Assertions.assertEquals(133.07, Math.round(sc.energy(3, 2)*100.0)/100.0);
+        Assertions.assertEquals(194.50, Math.round(sc.energy(4, 3)*100.0)/100.0);
     }
     
 }
